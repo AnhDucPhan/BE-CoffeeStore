6d4e77b0007a5cd897f4bafebffaed60c0f6a718
AnhDucPhan <phanducanh21032003@gmail.com>
2026-01-30 01:53:26 +0700

    update login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -25,7 +25,9 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { email: user.email, sub: user.id, role: user.role };
+    const { password, ...userInfo } = user;
     return {
+      user: userInfo,
       access_token: this.jwtService.sign(payload),
     };
   }
